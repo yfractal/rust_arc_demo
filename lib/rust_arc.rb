e637c6fe37e898410c17d5d3f4b7eb720cd2e3ab
Mike Yang <yfractal@gmail.com>
2024-06-08 14:55:55 +0800

    feat: init

---
+++ lib/rust_arc.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rust_arc/version"
+
+module RustArc
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rust_arc/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RustArc
+  VERSION = "0.1.0"
+end
